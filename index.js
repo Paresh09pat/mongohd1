8e034bc7e5281d7577d62666ba43a210b1fb9997
Paresh <patilpareshbaluchopda@gmail.com>
2022-11-27 16:12:30 +0530

    dbmongo

---
+++ index.js
@@ -0,0 +1,24 @@
+const {MongoClient} = require ('mongodb');
+const url = "mongodb://127.0.0.1:27017/";
+
+const client = new MongoClient(url);
+
+async function main(){
+    await client.connect();
+console.log('client connected...');
+
+    const database = client.db('Human_Resource');
+
+   const customer=  database.collection('employee');
+
+   const customerData = await customer.findOne({
+"firstName":"Jame",
+"lastName":"Jame",
+
+   });
+
+   console.log('find responce');
+   console.log(customerData);
+}
+
+main();
